012c24721c4e467c6bd52d8144f30c0646e2d230
Kamil Mierzejewski <kamil.mierzejewski@nokia.com>
2020-07-22 13:51:25 +0200

    ExpressionIndex extracted to a separate project.

--- django_native_search/models.py
+++ django_native_search/models.py
@@ -1,62 +1,17 @@
 from django.db import models
 from django.db.models.functions import Lower
-from django.db.backends.utils import names_digest, split_identifier
+import django_expression_index
 
 from django.template.loader import render_to_string
 from .manager import IndexManager
 from django.utils.functional import cached_property
 
 
-class ExpressionIndex(models.Index):
-    def __init__(self, *, expressions=(), name=None, db_tablespace=None, opclasses=(), condition=None):
-        super().__init__(fields=[str(e) for e in expressions], 
-                         name=name, db_tablespace=db_tablespace, 
-                         opclasses=opclasses, condition=condition)
-        self.expressions=expressions
-    
-    def deconstruct(self):
-        path, args, kwargs = super().deconstruct()
-        kwargs.pop('fields')
-        kwargs['expressions'] = self.expressions
-        return path, args, kwargs
-    
-    def set_name_with_model(self, model):
-        self.fields_orders=[(model._meta.pk.name,'')]
-        _, table_name = split_identifier(model._meta.db_table)
-        digest=names_digest(*self.fields, length=6)
-        self.name=f"{table_name[:19]}_{digest}_{self.suffix}"
-        
-    def create_sql(self, model, schema_editor, using='', **kwargs):
-        
-        class Descriptor:
-            db_tablespace=''
-            def __init__(self, expression):
-                self.column=str(expression)
-        
-        col_suffixes = [''] * len(self.expressions)
-        condition = self._get_condition_sql(model, schema_editor)
-        statement= schema_editor._create_index_sql(
-            model, [Descriptor(e) for e in self.expressions], 
-            name=self.name, using=using, db_tablespace=self.db_tablespace,
-            col_suffixes=col_suffixes, opclasses=self.opclasses, condition=condition,
-            **kwargs,
-        )
-        
-        compiler=model._meta.default_manager.all().query.get_compiler(connection=schema_editor.connection)
-        statement.parts['columns'] = ", ".join(
-            [self.compile_expression(e, compiler) for e in self.expressions])
-        return statement
-    
-    def compile_expression(self, expression, compiler):
-        expression=expression.resolve_expression(compiler.query, allow_joins=False)
-        sql, params=expression.as_sql(compiler, compiler.connection)
-        return sql % params
-
 class Lexem(models.Model):
     surface=models.CharField(max_length=255, db_index=True, unique=True)
     
     class Meta:
-        indexes=[ExpressionIndex(expressions=[Lower('surface')])]
+        indexes=[django_expression_index.ExpressionIndex(expressions=[Lower('surface')])]
     
     def __str__(self):
         return self.surface
